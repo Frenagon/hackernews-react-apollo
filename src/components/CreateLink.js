b88a1f471e7065dae8135a57fd90f3b930648b42
Frenagon <frenagon@gmail.com>
2021-02-13 22:39:02 -0600

    Implement Voting and Caching

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router";
 import { useMutation, gql } from "@apollo/client";
+import { LINKS_PER_PAGE } from "../constants";
+import { FEED_QUERY } from "./LinkList";
 
 const CREATE_LINK_MUTATION = gql`
   mutation PostMutation($description: String!, $url: String!) {
@@ -25,6 +27,34 @@ const CreateLink = () => {
     variables: {
       description: formState.description,
       url: formState.url,
+    },
+    update: (cache, { data: { post } }) => {
+      const take = LINKS_PER_PAGE;
+      const skip = 0;
+      const orderBy = { createdAt: "desc" };
+
+      const data = cache.readQuery({
+        query: FEED_QUERY,
+        variables: {
+          take,
+          skip,
+          orderBy,
+        },
+      });
+
+      cache.writeQuery({
+        query: FEED_QUERY,
+        data: {
+          feed: {
+            links: [post, ...data.feed.links],
+          },
+        },
+        variables: {
+          take,
+          skip,
+          orderBy,
+        },
+      });
     },
     onCompleted: () => history.push("/"),
   });
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const AUTH_TOKEN = "aith-token";
+export const LINKS_PER_PAGE = 10;
